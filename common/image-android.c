TinkerBoard2/u-boot

sdboot: dyanmic booting from eMMC or SD card (1/2)
Change-Id: Ibf2d42b78e693934536e3686da9031387a01044b
Signed-off-by: Rock Shen <rock_shen@asus.com>

common/image-android.c
@@ -153,9 +153,14 @@ int android_image_get_kernel(const struct andr_img_hdr *hdr, int verify,
 		len += strlen(cmdline);
 	}
 
+	char *rootmmc1 = "root=/dev/mmcblk1p8"; /* SDcard Boot */
+	char *rootmmc2 = "root=/dev/mmcblk2p8"; /* eMMC Boot */
+
 	char *bootargs = env_get("bootargs");
 	if (bootargs)
 		len += strlen(bootargs);
+
+	len += strlen(rootmmc1);
 
 	char *newbootargs = malloc(len + 2);
 	if (!newbootargs) {
@@ -170,6 +175,15 @@ int android_image_get_kernel(const struct andr_img_hdr *hdr, int verify,
 	}
 	if (cmdline)
 		strcat(newbootargs, cmdline);
+
+	char *devnum = env_get("devnum");
+	if (!strcmp(devnum, "0")) {
+		printf("devnum=0, boot from eMMC\n");
+		strcat(newbootargs, rootmmc2);
+	} else if (!strcmp(devnum, "1")) {
+		printf("devnum=1, boot from SD\n");
+		strcat(newbootargs, rootmmc1);
+	}
 
 	env_set("bootargs", newbootargs);
 

daikuei/rockchip-linux-u-boot

Change-Id: Iae69c69de8b3ed8014cabbdf07e96373bd94faa7

common/image-android.c
@@ -153,9 +153,14 @@ int android_image_get_kernel(const struct andr_img_hdr *hdr, int verify,
 		len += strlen(cmdline);
 	}
 
+	char *rootmmc1 = "root=/dev/mmcblk1p8"; /* SDcard Boot */
+	char *rootmmc2 = "root=/dev/mmcblk0p8"; /* eMMC Boot */
+
 	char *bootargs = env_get("bootargs");
 	if (bootargs)
 		len += strlen(bootargs);
+
+	len += strlen(rootmmc1);
 
 	char *newbootargs = malloc(len + 2);
 	if (!newbootargs) {
@@ -170,6 +175,15 @@ int android_image_get_kernel(const struct andr_img_hdr *hdr, int verify,
 	}
 	if (cmdline)
 		strcat(newbootargs, cmdline);
+
+	char *devnum = env_get("devnum");
+	if (!strcmp(devnum, "0")) {
+		printf("devnum=0, boot from eMMC\n");
+		strcat(newbootargs, rootmmc2);
+	} else if (!strcmp(devnum, "1")) {
+		printf("devnum=1, boot from SD\n");
+		strcat(newbootargs, rootmmc1);
+	}
 
 	env_set("bootargs", newbootargs);
 